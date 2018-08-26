d224e9fef4a4af84c53c5329d82866764e1e68f9
SpkCoder <1533165085@qq.com>
2018-08-26 18:00:20 +0800

    xx

--- ecast-mongo/config/development.js
+++ ecast-mongo/config/development.js
@@ -1,7 +1,7 @@
 //development.js
 //"dburl": "mongodb://test:123456@192.168.218.145:27017/test"
 module.exports = {
-	  "dburl": "mongodb://ecast:123456@192.168.216.85:27017/ecast"
+	  "dburl": "mongodb://localhost:27017/ecastDB"
 
 };
 
--- nodeApp-LayUI/config/development.js
+++ nodeApp-LayUI/config/development.js
@@ -1,4 +1,5 @@
 //development.js
+//"dburl": "mongodb://test:123456@192.168.218.145:27017/test"
 module.exports = {
 	  "dburl": "mongodb://localhost:27017/mydb"
 

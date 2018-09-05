917b612b89734abccf8005e737f0d3b3e4e476a7
Berg Yu <berg.yu@atck.io>
2018-09-05 12:46:40 +0800

    xx

--- ecast-mongo/config/development.js
+++
@@ -1,7 +0,0 @@
-//development.js
-//"dburl": "mongodb://test:123456@192.168.218.145:27017/test"
-module.exports = {
-	  "dburl": "mongodb://localhost:27017/ecastDB"
-
-};
-
--- ecast-mongo/config/production.js
+++
@@ -1,5 +0,0 @@
-//production.js
-module.exports = {
-	  "dburl": "mongodb://test:123456@192.168.218.145:27017/test"
-
-};
